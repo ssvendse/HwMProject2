9f8abe4dc9b11cbab7f4c874c3bc12f7bc8b41e8
Skyler Svendsen <skylersvendsen@gmail.com>
2018-01-11 22:40:34 -0600

    first commit

--- HwMProject2/ViewController.swift
+++ HwMProject2/ViewController.swift
@@ -7,21 +7,126 @@
 //
 
 import Cocoa
+import GameplayKit
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    
+    @IBOutlet var tableView: NSTableView!
+    @IBOutlet var guess: NSTextField!
+    
+    var answer = ""
+    var guesses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        startNewGame()
     }
-
+    
+    @IBAction func submitGuess(_ sender: Any) {
+        let guessString = guess.stringValue
+        guard Set(guessString).count == 4 else { return }
+        
+        let badCharacters = CharacterSet(charactersIn: "0123456789").inverted
+        guard guessString.rangeOfCharacter(from: badCharacters) == nil else { return }
+        
+        guesses.insert(guessString, at: 0)
+        tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
+        
+        //winning logic
+        let resultString = result(for: guessString)
+        
+        if resultString.contains("4b") {
+            let alert = NSAlert()
+            alert.messageText = "You win!"
+            alert.informativeText = "Congratulations! Click OK to play again."
+            alert.runModal()
+            
+            startNewGame()
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
+    
+    func result(for guess: String) -> String {
+        var bulls = 0
+        var cows = 0
+        
+        let guessLetters = Array(guess)
+        let answerLetters = Array(answer)
+        
+        for (index, letter) in guessLetters.enumerated() {
+            if letter == answerLetters[index] {
+                bulls += 1
+            } else if answerLetters.contains(letter) {
+                cows += 1
+            }
+        }
+        
+        return "\(bulls)b \(cows)c"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        
+        if tableColumn?.title == "Guess" {
+            //this is the guess column
+            vw.textField?.stringValue = guesses[row]
+        } else {
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+    
+    func startNewGame() {
+        guess.stringValue = ""
+        guesses.removeAll()
+        answer = ""
+        
+        var numbers = Array(0...9)
+        numbers = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: numbers) as! [Int]
+        
+        for _ in 0 ..< 4 {
+            answer.append(String(numbers.removeLast()))
+        }
+        
+        tableView.reloadData()
+    }
 
-
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
